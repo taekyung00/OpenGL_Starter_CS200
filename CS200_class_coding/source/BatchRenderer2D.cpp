02fe493400a8f98f3d1245284966267cd2527646
taekyung00 <103232596+taekyung00@users.noreply.github.com>
2025-10-27 02:08:32 +0900

    Update BatchRenderer2D.cpp

--- CS200_class_coding/source/BatchRenderer2D.cpp
+++ CS200_class_coding/source/BatchRenderer2D.cpp
@@ -23,10 +23,11 @@ void BatchRenderer2D::Init()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(QuadVertex) * maxVertices, nullptr, GL_DYNAMIC_DRAW); // every frame we'll update our buffer
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	//setup index buffer
-	//unlike vertex buffer that gonna change every frame, index don't change
-	//i.e. 0 1 2 2 3 0 ... << this pattern repeat
-	//but just amount of index gonna change(how many do we need?)
+	// setup index buffer
+
+	// unlike vertex buffer that gonna change every frame, index don't change
+	// i.e. 0 1 2 2 3 0 ... << this pattern repeat
+	// but just amount of index gonna change(how many do we need?)
 	std::vector<unsigned> indice_values(maxIndices);
 	unsigned			  offset = 0;
 	for (unsigned i = 0; i < maxIndices; i += 6)
@@ -38,4 +39,71 @@ void BatchRenderer2D::Init()
 		indice_values[i + 4] = offset + 3;
 		indice_values[i + 5] = offset + 0;
 	}
+
+	glGenBuffers(1, &indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned) * maxIndices, indice_values.data(), GL_STATIC_DRAW); // sizeof(unsigned) * maxIndices == sizeof(indice_values)
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// Create vertex array object
+	glGenVertexArrays(1, &vertexArrayObject);
+	glBindVertexArray(vertexArrayObject);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexBuffer);
+
+	// Position attribute (location 0)
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(QuadVertex), nullptr);
+	glVertexAttribDivisor(0, 0);
+
+	// Texture coordinate attribute (location 1)
+	glEnableVertexAttribArray(1);
+	const ptrdiff_t texcoord_offset = 2 * sizeof(float);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(QuadVertex), reinterpret_cast<void*>(texcoord_offset));
+	glVertexAttribDivisor(1, 0);
+
+	// Tint attribute (location 2)
+	glEnableVertexAttribArray(2);
+	// const ptrdiff_t tint_offset = 4 * sizeof(float);
+	const ptrdiff_t tint_offset = offsetof(QuadVertex, tint); // automatically calculate offset!
+	glVertexAttribPointer(2, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(QuadVertex), reinterpret_cast<void*>(tint_offset));
+	glVertexAttribDivisor(2, 0);
+
+	// Unbind VAO and buffers
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// Enable blending for transparency
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDisable(GL_DEPTH_TEST);
+}
+
+void BatchRenderer2D::Shutdown()
+{
+	glDeleteVertexArrays(1, &vertexArrayObject);
+	glDeleteBuffers(1, &vertexBuffer);
+	glDeleteBuffers(1, &indexBuffer);
+	glDeleteProgram(shader.Shader);
+}
+
+void BatchRenderer2D::BeginScene(std::span<const float, 9> ndc_matrix)
+{
+	glUseProgram(shader.Shader);
+	glUniformMatrix3fv(shader.UniformLocations.at("uToNDC"), 1, GL_FALSE, ndc_matrix.data());
+	glUseProgram(0);
+
+	startBatch();
+}
+
+void BatchRenderer2D::EndScene()
+{
+	flush();
+}
+
+void BatchRenderer2D::startBatch()
+{
+	vertexDataEnd = vertexData.data();
+	indexCount	  = 0;
 }
